b140d343faa6cbc58bac30bd8cbf538bbc4736fd
DiegoArmira <diego28shil@gmail.com>
2019-08-26 21:30:49 -0600

    Commit que incorpora la función multiplicación y división. La función división no considera el erros de división entre cero. 201220159

--- Funciones.java
+++ Funciones.java
@@ -25,4 +25,15 @@ public class Funciones {
 	public double raizAlCubo(double valor){
 		return Math.pow(valor, 1.0/3.0);
 	}
+
+	public double multiplicacion(double valor1, double valor2)
+	{
+		return valor1*valor2;
+	}
+
+	public double division(double valor1, double valor2)
+	{
+		//Función de división sin validar la división entre cero;
+		return valor1/valor2;
+	}
 }
