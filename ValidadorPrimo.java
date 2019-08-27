a6a828e9173e8efbe0c055c43fa10fcf27d9f099
pensa <fernanpensa@gmail.com>
2019-08-27 10:59:20 -0600

    agrengando en numero primo por que numeros es divisible si no es primo 201602743

--- ValidadorPrimo.java
+++ ValidadorPrimo.java
@@ -3,11 +3,13 @@ public class ValidadorPrimo{
 		if(n <= 0){
 			return false;
 		}
+		boolean todook = true;
 		for(int a = 2; a < n / 2; a++){
 			if(n % a == 0){
-				return false;
+				todook = false;
+				System.out.println(n + " es divisible dentro de " + a);
 			}
 		}
-		return true;
+		return todook;
 	}
 }
